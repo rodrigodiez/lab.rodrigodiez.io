d7fb835925546458e34bacfab2a11750dfe0c485
Rodrigo Díez Villamuera <rodrigo@rodrigodiez.io>
2020-09-21 15:59:35 +0100

    Included react-boostrap, customised front page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Jumbotron, Button } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jumbotron>
+        <h1>Welcome to the Lab!</h1>
+        <p>This page will contain a directory of site projects</p>
+      </Jumbotron>
     </div>
   );
 }
